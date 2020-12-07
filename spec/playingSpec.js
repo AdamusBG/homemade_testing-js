3152e7208305113b8df415421710d4c35fdec6b8
Adam G <adamwizzy@hotmail.co.uk>
2020-12-07 17:39:54 +0000

    created new expectation function for comparing arrays

--- spec/playingSpec.js
+++ spec/playingSpec.js
@@ -1,3 +1,8 @@
 it("Returns 6 when 4 and 2 added together", expect(addTwoNumbers(4, 2)).toEqual(6));
 
 it("Returns 5 when 4 and 2 added together", expect(addTwoNumbers(4, 2)).toEqual(5));
+
+
+it("Empty array equals empty array", expect([]).toEqualArray([]));
+
+it("Array can be matched against same array", expect([1,2]).toEqualArray([1,2]));
